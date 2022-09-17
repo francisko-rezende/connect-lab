c409f6067b409c34382745d309a5866791558271
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-17 19:08:06 -0300

    feat: create placeholder files por Devices, Home, and Profile pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,5 @@
+export { Home } from "./Home";
+export { Profile } from "./Profile";
+export { Devices } from "./Devices";
 export { Login } from "./Login";
 export { Registration } from "./Registration";
