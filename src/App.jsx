287647fc4b60d4dab51425ccfe7cac0513db65d5
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-17 18:42:22 -0300

    feat: implement router and its encompassing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { Registration } from "@pages"
-
+import { Registration } from "@pages";
+import { Link, RouterProvider } from "react-router-dom";
+import { Router } from "@router";
 function App() {
-
-  return <Registration />
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>Título</h1>
+        </nav>
+      </header>
+      <main>
+        <Router />
+      </main>
+      <footer>
+        <p>Feito com 😠 por Francisko</p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
