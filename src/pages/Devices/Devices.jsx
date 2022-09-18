9751afff080260324d0e3c4accfa08ac4ece9537
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-18 11:41:16 -0300

    feat: add initial version of registered devices data fetch

--- src/pages/Devices/Devices.jsx
+++ src/pages/Devices/Devices.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { devices } from "../../assets/devices";
+import { axiosInstance } from "@lib/axios";
+import { useState } from "react";
 
 export const Devices = () => {
-  return <h1>Devices</h1>;
+  // const [devices, setDevices] = useState(null);
+  // useState(async () => {
+  //   const { data } = await axiosInstance.get("devices");
+  //   setDevices(data);
+  // }, []);
+
+  return (
+    <>
+      <h1>Devices</h1>
+      <ul>
+        {devices
+          ? devices.map(({ name, _id }) => <li key={_id}>{name}</li>)
+          : "Carregando..."}
+      </ul>
+    </>
+  );
 };
