e1e71a49265bbefa312ebca2804dc857c6bc5e98
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-18 14:50:10 -0300

    feat: integrate initial version of user device list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable camelcase */
 import { axiosInstance } from "@lib/axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
@@ -6,11 +8,18 @@ export const Home = () => {
   //   const res = await axiosInstance.get(`/users/631b6e3e61ef65fb3859152f`);
   // };
 
-  // const getUserDevices = async () => {
-  //   const res = await axiosInstance.get(
-  //     `/userDevices/user/631b6e3e61ef65fb3859152f`,
-  //   );
-  // };
+  const [userDevices, setUserDevices] = useState(null);
+
+  const getUserDevices = async (id) => {
+    const res = await axiosInstance.get(`/userDevices/user/${id}`);
+    return res;
+  };
+
+  useEffect(() => {
+    getUserDevices("632729c69bc4141442d087fa").then((res) =>
+      setUserDevices(res.data),
+    );
+  }, []);
 
   return (
     <>
@@ -19,9 +28,15 @@ export const Home = () => {
         <Link to={"devices"}>Devices</Link>
         <Link to={"perfil"}>Perfil</Link>
       </div>
-      <div>
+      <ul>
+        {userDevices &&
+          userDevices.map(({ device: { name, _id }, is_on }) => (
+            <li key={_id}>
+              {name} / tá on? {is_on ? "Sim" : "Nada"}
+            </li>
+          ))}
         {/* <button onClick={getUserDevices}>Devices do usuário</button> */}
-      </div>
+      </ul>
     </>
   );
 };
