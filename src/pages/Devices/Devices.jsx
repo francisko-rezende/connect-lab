ee78e4da0e05e53d92e595fc0c5aa2be781cbbdb
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-18 14:54:48 -0300

    feat: integrate initial version of overall registered device list

--- src/pages/Devices/Devices.jsx
+++ src/pages/Devices/Devices.jsx
@@ -1,22 +1,47 @@
-import { devices } from "../../assets/devices";
+// import { devices } from "../../assets/devices";
 import { axiosInstance } from "@lib/axios";
 import { useState } from "react";
 
 export const Devices = () => {
-  // const [devices, setDevices] = useState(null);
-  // useState(async () => {
-  //   const { data } = await axiosInstance.get("devices");
-  //   setDevices(data);
-  // }, []);
+  const [devices, setDevices] = useState(null);
+  useState(async () => {
+    const { data } = await axiosInstance.get("devices");
+    setDevices(data);
+  }, []);
 
   return (
     <>
       <h1>Devices</h1>
       <ul>
         {devices
-          ? devices.map(({ name, _id }) => <li key={_id}>{name}</li>)
+          ? devices.map(({ name, _id }) => (
+              <li key={_id}>
+                {name}{" "}
+                <button
+                  onClick={() =>
+                    axiosInstance.post("/userDevices", {
+                      user: "632729c69bc4141442d087fa",
+                      device: _id,
+                      is_on: false,
+                      local: "631b348a6f2d2f24a7c0c962",
+                      room: "Sauna",
+                    })
+                  }
+                >
+                  Adicionar
+                </button>
+              </li>
+            ))
           : "Carregando..."}
       </ul>
     </>
   );
 };
+
+// const deviceToAdd = {
+//   user: "632729c69bc4141442d087fa",
+//   device: _id,
+//   is_on: false,
+//   local: "631b348a6f2d2f24a7c0c962",
+//   room: "Sauna",
+// };
