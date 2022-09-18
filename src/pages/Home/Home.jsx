81aff43dafa523d1e3b511bc88562599ec2d8ba8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-18 11:48:13 -0300

    feat: create initial version of userDevices data fetch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
+import { axiosInstance } from "@lib/axios";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+  // const getUser = async () => {
+  //   const res = await axiosInstance.get(`/users/631b6e3e61ef65fb3859152f`);
+  // };
+
+  // const getUserDevices = async () => {
+  //   const res = await axiosInstance.get(
+  //     `/userDevices/user/631b6e3e61ef65fb3859152f`,
+  //   );
+  // };
+
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to={"devices"}>Devices</Link>
-      <Link to={"perfil"}>Perfil</Link>
-    </div>
+    <>
+      <div>
+        <h1>Home</h1>
+        <Link to={"devices"}>Devices</Link>
+        <Link to={"perfil"}>Perfil</Link>
+      </div>
+      <div>
+        {/* <button onClick={getUserDevices}>Devices do usuário</button> */}
+      </div>
+    </>
   );
 };
