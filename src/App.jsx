4161f8897eaba7bd05891554dfccd1daf1fd0874
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-18 07:09:05 -0300

    feat: add conditional rendering to app header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Registration } from "@pages";
-import { Link, RouterProvider } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Router } from "@router";
+import { useAuth } from "@hooks";
 function App() {
+  const { hasSignedIn, toggleSignedIn } = useAuth();
+  const navigate = useNavigate();
   return (
     <>
       <header>
+        {hasSignedIn ? "Tá logado" : "Não tá ligado"}
+        <button onClick={toggleSignedIn}>
+          {hasSignedIn ? "Deslogar" : "Logar"}
+        </button>
+        <button onClick={() => navigate("/")}>ir pra home</button>
         <nav>
           <h1>Título</h1>
         </nav>
