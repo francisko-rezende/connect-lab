0610f2a0b22c5e80e42648a9b6cfa2a91f87acac
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-18 07:05:19 -0300

    feat: add Links to Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to={"devices"}>Devices</Link>
+      <Link to={"perfil"}>Perfil</Link>
+    </div>
+  );
 };
