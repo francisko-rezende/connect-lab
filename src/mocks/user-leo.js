59ffbf9efe7f01cebb64458a7b47190a1883a648
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-20 07:20:08 -0300

    feat: add user and registered devices mock data

---
+++ src/mocks/user-leo.js
@@ -0,0 +1,19 @@
+const leo = {
+  _id: "632729c69bc4141442d087fa",
+  fullName: "Léo Stronda",
+  photoUrl: "https://i.imgur.com/9XncnrC.jpg",
+  email: "leo@stronda.com",
+  password: "",
+  phone: "35998282456",
+  userAddress: {
+    zipCode: "23548-063",
+    street: "Travessa 13",
+    number: "69",
+    neighborhood: "Sepetiba",
+    city: "Rio de Janeiro",
+    state: "RJ",
+    complement: "Mansão Thug Stronda",
+    _id: "632729c69bc4141442d087fb",
+  },
+  __v: 0,
+};
