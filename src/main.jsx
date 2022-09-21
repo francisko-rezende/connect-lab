ec31715d4941b38c668ffdc353a95d0eb8130782
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-21 06:55:39 -0300

    feat: wrap app with query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { AuthProvider } from "@contexts";
+import { queryClient } from "@lib/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
