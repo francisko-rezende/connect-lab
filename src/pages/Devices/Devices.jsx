466c10237a3038445b09cdf2a169608f9e6a5b6b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-22 09:09:37 -0300

    feat: implement initial version of devices page with react query hooks

--- src/pages/Devices/Devices.jsx
+++ src/pages/Devices/Devices.jsx
@@ -1,47 +1,31 @@
-// import { devices } from "../../assets/devices";
-import { axiosInstance } from "@lib/axios";
-import { useState } from "react";
+import { useAddUserDevice, useRegisteredDevices, useUserDevices } from "@hooks";
+import { queryClient } from "@lib/react-query";
 
 export const Devices = () => {
-  const [devices, setDevices] = useState(null);
-  useState(async () => {
-    const { data } = await axiosInstance.get("devices");
-    setDevices(data);
-  }, []);
+  const devices = useRegisteredDevices();
+  const addUserDevice = useAddUserDevice();
+  const userId = queryClient.getQueryData("user")._id;
+  useUserDevices(userId);
 
   return (
     <>
       <h1>Devices</h1>
       <ul>
-        {devices
-          ? devices.map(({ name, _id }) => (
+        {devices.isLoading
+          ? "Carregando"
+          : devices.data.map(({ name, _id }) => (
               <li key={_id}>
                 {name}{" "}
                 <button
                   onClick={() =>
-                    axiosInstance.post("/userDevices", {
-                      user: "632729c69bc4141442d087fa",
-                      device: _id,
-                      is_on: false,
-                      local: "631b348a6f2d2f24a7c0c962",
-                      room: "Sauna",
-                    })
+                    addUserDevice.mutate({ userId, deviceId: _id })
                   }
                 >
                   Adicionar
                 </button>
               </li>
-            ))
-          : "Carregando..."}
+            ))}
       </ul>
     </>
   );
 };
-
-// const deviceToAdd = {
-//   user: "632729c69bc4141442d087fa",
-//   device: _id,
-//   is_on: false,
-//   local: "631b348a6f2d2f24a7c0c962",
-//   room: "Sauna",
-// };
