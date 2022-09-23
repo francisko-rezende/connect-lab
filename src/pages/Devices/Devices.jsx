c92ae6033ac18c60551f0b54676148e1d5d1377b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-22 21:47:06 -0300

    feat: start using userId from globalContext in Devices

--- src/pages/Devices/Devices.jsx
+++ src/pages/Devices/Devices.jsx
@@ -1,10 +1,14 @@
-import { useAddUserDevice, useRegisteredDevices, useUserDevices } from "@hooks";
-import { queryClient } from "@lib/react-query";
+import {
+  useAddUserDevice,
+  useGlobalContext,
+  useRegisteredDevices,
+  useUserDevices,
+} from "@hooks";
 
 export const Devices = () => {
   const devices = useRegisteredDevices();
   const addUserDevice = useAddUserDevice();
-  const userId = queryClient.getQueryData("user")._id;
+  const { userId } = useGlobalContext();
   useUserDevices(userId);
 
   return (
