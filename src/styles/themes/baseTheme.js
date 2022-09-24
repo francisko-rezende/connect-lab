8b8be60b81e5178f1758871e214af75f40f274fb
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-24 13:36:10 -0300

    feat: create base theme

---
+++ src/styles/themes/baseTheme.js
@@ -0,0 +1,25 @@
+export const baseTheme = {
+  font: {
+    weight: {
+      normal: 400,
+      bold: 600,
+    },
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.4rem",
+      medium: "1.6rem",
+      large: "1.8rem",
+      xlarge: "2.0rem",
+      xxlarge: "2.8rem",
+    },
+  },
+  spacings: {
+    xxsmall: "0.8rem",
+    xsmall: "1.6rem",
+    small: "2.4rem",
+    medium: "3.2rem",
+    large: "4.0rem",
+    xlarge: "4.8rem",
+    xxlarge: "5.6rem",
+  },
+};
