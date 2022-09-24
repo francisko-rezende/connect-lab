8bc884bdcc8fe06775606b93a4777c9e9026ab4d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-24 13:42:51 -0300

    feat: add theme provider to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import { useNavigate } from "react-router-dom";
 import { Router } from "@router";
-import { useAuth } from "@hooks";
+import { useAuth, useGlobalContext } from "@hooks";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "@styles";
+
 function App() {
   const navigate = useNavigate();
   const { token, signOut } = useAuth();
+  const { theme } = useGlobalContext();
+  const currentTheme = theme === "dark" ? darkTheme : lightTheme;
 
   return (
     <>
-      <header>
-        {token ? "Tá logado" : "Não tá ligado"}
-        <button onClick={() => navigate("/")}>ir pra home</button>
-        {token && <button onClick={signOut}>Deslogar</button>}
-
-        <nav>
-          <h1>Título</h1>
-        </nav>
-      </header>
-      <main>
-        <Router />
-      </main>
-      <footer>
-        <p>Feito com 😠 por Francisko</p>
-      </footer>
+      <ThemeProvider theme={currentTheme}>
+        <header>
+          <button onClick={() => navigate("/")}>ir pra home</button>
+          {token && <button onClick={signOut}>Deslogar</button>}
+          <p>Tema atual: {theme}</p>
+          <nav>
+            <h1>Título</h1>
+          </nav>
+        </header>
+        <main>
+          <Router />
+        </main>
+        <footer>
+          <p>Feito com 😠 por Francisko</p>
+        </footer>
+      </ThemeProvider>
     </>
   );
 }
