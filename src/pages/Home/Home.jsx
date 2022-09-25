4eed626c3b33b1234f47b72eccb9fe85999c4017
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-25 20:00:54 -0300

    feat: implement filter and device dialog to home screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable camelcase */
 import { getRegisteredDevices, getUser } from "@api";
-import { Container, Grid } from "@components";
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogContent,
+  Grid,
+  StatusIcon,
+} from "@components";
 import * as S from "./Home.styles";
 import {
   useUserDevices,
@@ -10,22 +17,26 @@ import {
   useCheckToken,
   useWeatherData,
   useUser,
+  useLocationOptions,
 } from "@hooks";
 import { queryClient } from "@lib/react-query";
-import { useEffect } from "react";
-import styled, { css } from "styled-components";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
   const { userId } = useGlobalContext();
+  const locations = useLocationOptions();
 
   const userQuery = useUser(userId);
   const weatherQuery = useWeatherData(userQuery.data?.userAddress?.city);
   queryClient.setQueryDefaults(["weather", userQuery.data?.userAddress?.city], {
-    // enabled: !!userQuery.data?.userAddress?.city,
+    enabled: !!userQuery.data?.userAddress?.city,
   });
-  const { userDevicesQuery } = useUserDevices(userId);
+  const userDevicesQuery = useUserDevices(userId);
   const removeDevice = useRemoveUserDevice();
   const toggleDeviceStatus = useToggleDeviceStatus();
+  const [isDialogOpen, setIsDialogOpen] = useState(true);
+
+  const [locationToShow, setLocationToShow] = useState(null);
 
   useCheckToken();
 
@@ -33,6 +44,19 @@ export const Home = () => {
     queryClient.prefetchQuery("registeredDevices", getRegisteredDevices);
     queryClient.prefetchQuery("user", () => getUser(userId));
   }, [userId]);
+
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
+
+  const filteredDevices = locationToShow
+    ? userDevicesQuery.data.filter(
+        ({ local: { description } }) => description === locationToShow,
+      )
+    : userDevicesQuery.data;
+
+  const selectedDevice = queryClient.getQueryData([
+    "userDevices",
+    selectedDeviceId,
+  ]);
 
   return (
     <Container>
@@ -59,52 +83,118 @@ export const Home = () => {
       ) : (
         <p>Loading...</p>
       )}
-
+      <S.FilterButtonsWrapper>
+        {!locations.isLoading &&
+          locations.data.map(({ description, _id }) => (
+            <Button
+              variant="regular"
+              key={_id}
+              onClick={() => setLocationToShow(description)}
+            >
+              {description}
+            </Button>
+          ))}
+        <Button variant="regular" onClick={() => setLocationToShow(null)}>
+          Todos
+        </Button>
+      </S.FilterButtonsWrapper>
       <Grid>
         {userDevicesQuery.isLoading ? (
           <p>Loading...</p>
         ) : (
-          userDevicesQuery.data.map(
-            ({
-              device: { name, photoUrl },
-              is_on,
-              _id,
-              room,
-              local: { description },
-            }) => {
-              return (
-                <li key={_id}>
-                  <S.DeviceCard>
-                    <S.ImgWrapper>
-                      <img src={photoUrl} alt={name} />
-                    </S.ImgWrapper>
-                    <S.DeviceInfoWrapper>
-                      <S.DeviceName>{name}</S.DeviceName>
-                      <S.DeviceInfoParagraph>
-                        {description} | {room} | {is_on ? "ON" : "OFF"}
-                      </S.DeviceInfoParagraph>
-                    </S.DeviceInfoWrapper>
-                    {/* {name} / tá on? {is_on ? "Sim" : "Nada"} */}
-                    <button
-                      onClick={() => {
-                        toggleDeviceStatus.mutate({
-                          deviceId: _id,
-                          deviceStatus: is_on,
-                        });
-                      }}
-                    >
-                      {is_on ? "Desliga" : "Liga"}
-                    </button>
-                    {/* <button onClick={() => removeDevice.mutate(_id)}>
-                      Remover
-                    </button> */}
-                  </S.DeviceCard>
-                </li>
-              );
-            },
-          )
+          filteredDevices.map((item) => {
+            return (
+              <li key={item._id}>
+                <S.DeviceCard
+                  role="button"
+                  onClick={() => {
+                    setSelectedDeviceId(item._id);
+                    setIsDialogOpen(true);
+                  }}
+                >
+                  <S.ImgWrapper>
+                    <img src={item.device.photoUrl} alt={item.device.name} />
+                  </S.ImgWrapper>
+                  <S.DeviceInfoWrapper>
+                    <S.DeviceName>{item.device.name}</S.DeviceName>
+                    <S.DeviceInfoParagraph>
+                      {item.local.description} | {item.room} |{" "}
+                      {item.is_on ? "ON" : "OFF"}
+                    </S.DeviceInfoParagraph>
+                  </S.DeviceInfoWrapper>
+                  <S.StatusButtonWrapper
+                    role="button"
+                    onClick={(ev) => {
+                      ev.stopPropagation();
+                      toggleDeviceStatus.mutate({
+                        deviceId: item._id,
+                        deviceStatus: item.is_on,
+                      });
+                    }}
+                  >
+                    <StatusIcon isOn={item.is_on} />
+                  </S.StatusButtonWrapper>
+                </S.DeviceCard>
+              </li>
+            );
+          })
         )}
       </Grid>
+
+      {selectedDevice && (
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+          <DialogContent>
+            <S.ContentWrapper>
+              <S.CustomDialogTitle>
+                {selectedDevice.device.name}
+              </S.CustomDialogTitle>
+              <S.H3>{selectedDevice.device.madeBy}</S.H3>
+              <S.ImgWrapper>
+                <img
+                  src={selectedDevice.device.photoUrl}
+                  alt={selectedDevice.device.name}
+                />
+              </S.ImgWrapper>
+
+              <S.StatusWrapper>
+                <S.Status>
+                  Dispositivo {selectedDevice.is_on ? "Ligado" : "Desligado"}
+                </S.Status>
+                <S.StatusButtonWrapper
+                  role="button"
+                  onClick={() => {
+                    toggleDeviceStatus.mutate({
+                      deviceId: selectedDevice._id,
+                      deviceStatus: selectedDevice.is_on,
+                    });
+                  }}
+                >
+                  <StatusIcon isOn={selectedDevice.is_on} />
+                </S.StatusButtonWrapper>
+              </S.StatusWrapper>
+
+              <S.H4>Informações do dispositivo</S.H4>
+              <S.DetailsWrapper>
+                <p>ID virtual: {selectedDevice.device.info.virtual_id}</p>
+                <p>Endereço IP: {selectedDevice.device.info.ip_address}</p>
+                <p>Endereço MAC: {selectedDevice.device.info.mac_address}</p>
+                <p>Força do sinal: {selectedDevice.device.info.signal}</p>
+              </S.DetailsWrapper>
+              <Button
+                variant="regular"
+                onClick={() =>
+                  removeDevice.mutate({
+                    id: selectedDevice._id,
+                    setIsDialogOpen,
+                  })
+                }
+              >
+                Remover
+              </Button>
+            </S.ContentWrapper>
+          </DialogContent>
+        </Dialog>
+      )}
     </Container>
   );
 };
--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -1,3 +1,4 @@
+import { DialogTitle } from "@components";
 import styled, { css } from "styled-components";
 
 export const Temperature = styled.p`
@@ -12,6 +13,7 @@ export const WeatherWrapper = styled.div`
   display: grid;
   place-items: center;
   padding: 20px;
+  margin-top: 60px;
   ${({ theme }) => css`
     background: ${theme.colors.gray.gray01};
     border-radius: ${theme.borderRadius};
@@ -39,10 +41,12 @@ export const DeviceName = styled.h3`
 `;
 
 export const DeviceCard = styled.div`
+  cursor: pointer;
   padding: 20px;
   display: grid;
   gap: 8px;
   grid-template-columns: 80px 1fr 40px;
+  align-items: center;
   ${({ theme }) => css`
     background: ${theme.colors.gray.gray01};
     border-radius: ${theme.borderRadius};
@@ -54,4 +58,61 @@ export const ImgWrapper = styled.div`
   width: 80px;
   border-radius: ${({ theme }) => theme.borderRadius};
   overflow: hidden;
+`;
+
+export const CustomDialogTitle = styled(DialogTitle)`
+  font-size: 1.25rem;
+  text-align: center;
+`;
+
+export const ContentWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  gap: 35px;
+`;
+
+export const H3 = styled.h3`
+  font-size: 1rem;
+  ${({ theme }) => css`
+    color: ${theme.colors.gray.gray11};
+  `}
+`;
+
+export const Status = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray.gray11};
+  `}
+`;
+
+export const StatusButtonWrapper = styled.div`
+  cursor: pointer;
+  width: 35px;
+  height: 35px;
+`;
+
+export const StatusWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 32px;
+`;
+
+export const H4 = styled.h4`
+  font-size: 1rem;
+  line-height: 2.5;
+  ${({ theme }) => css`
+    color: ${theme.colors.gray.gray12};
+    border-bottom: 1px solid ${theme.colors.gray.gray12};
+  `}
+`;
+
+export const DetailsWrapper = styled.div``;
+
+export const FilterButton = styled.button`
+  ${({ theme, isSelected }) => css``}
+`;
+
+export const FilterButtonsWrapper = styled.div`
+  margin-top: 32px;
+  display: flex;
+  gap: 8px;
 `;
