d9ed6fe207d21ff2f78d47000e26eaaab954c546
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-25 09:59:00 -0300

    feat: make Button able to receive variant prop to set its style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as S from "./Button.styles";
 
-export const Button = ({ children }) => {
-  return <S.Button>{children}</S.Button>;
+export const Button = ({ children, variant }) => {
+  return <S.Button variant={variant}>{children}</S.Button>;
 };
 
 Button.propTypes = {
   children: PropTypes.node,
+  variant: PropTypes.string,
 };
