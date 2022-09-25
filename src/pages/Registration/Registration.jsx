e3ae5cd135884d57d6d5900ba123ac7770768a66
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-24 22:19:44 -0300

    feat: style registration page

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as S from "./Registration.styles.js";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Link } from "react-router-dom";
 import { axiosInstance } from "@lib/axios";
 import { formSchema, validatorRegex } from "@lib/yup";
+import { Button, Container, InputWrapper } from "@components";
 
 export const Registration = () => {
   const {
     register,
     handleSubmit,
-    reset,
     setValue,
     formState: { errors },
   } = useForm({ resolver: yupResolver(formSchema) });
@@ -29,178 +28,169 @@ export const Registration = () => {
   };
 
   return (
-    <>
-      <h2>Cadastrar</h2>
-      <S.Form onSubmit={handleSubmit(handleRegisterUser)}>
-        <div>
-          <label htmlFor="fullName">Nome completo*</label>
-          <input
-            type="text"
-            name="fullName"
-            id="fullName"
-            {...register("fullName")}
-          />
-          {errors.fullName && <span>{errors.fullName.message}</span>}
-        </div>
-
-        <div>
-          <label htmlFor="email">E-mail*</label>
-          <input type="text" name="email" id="email" {...register("email")} />
-          {errors.email && <span>{errors.email.message}</span>}
-        </div>
-
-        <div>
-          <label htmlFor="photoUrl">URL foto de perfil</label>
-          <input
-            type="text"
-            name="photoUrl"
-            id="photoUrl"
-            {...register("photoUrl")}
-          />
-          {errors.photoUrl && <span>{errors.photoUrl.message}</span>}
-        </div>
-
-        <div>
-          <label htmlFor="phone">Telefone*</label>
-          <input type="phone" name="phone" id="phone" {...register("phone")} />
-          {errors.phone && <span>{errors.phone.message}</span>}
-        </div>
-
-        <div>
-          <label htmlFor="password">Senha*</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            {...register("password")}
-          />
-          {errors.password && <span>{errors.password.message}</span>}
-        </div>
-
-        <div>
-          <label htmlFor="passwordConfirmation">Confirmação de senha*</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            {...register("confirmPassword")}
-          />
-          {errors.confirmPassword && (
-            <span>{errors.confirmPassword.message}</span>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="zipCode">CEP*</label>
-          <input
-            type="text"
-            name="zipCode"
-            id="zipCode"
-            {...register("userAddress.zipCode", {
-              onBlur: (e) => {
-                const zipCode = e.target.value;
-                const isValidZip = validatorRegex.zipCode.test(zipCode);
-                if (isValidZip) {
-                  const numbersOnlyZip = zipCode.replace(/\D/g, "");
-                  fetch(`https://viacep.com.br/ws/${numbersOnlyZip}/json/`)
-                    .then((response) => response.json())
-                    .then(({ bairro, localidade, logradouro, uf }) => {
-                      setValue("userAddress.neighborhood", bairro);
-                      setValue("userAddress.city", localidade);
-                      setValue("userAddress.street", logradouro);
-                      setValue("userAddress.state", uf);
-                    });
-                }
-              },
-            })}
-          />
-          {errors.userAddress?.zipCode?.message && (
-            <span>{errors.userAddress?.zipCode?.message}</span>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="street">Logradouro/Endereço*</label>
-          <input
-            type="text"
-            name="street"
-            id="street"
-            {...register("userAddress.street")}
-          />
-          {errors.userAddress?.street?.message && (
-            <span>{errors.userAddress?.street.message}</span>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="city">Cidade*</label>
-          <input
-            type="text"
-            name="city"
-            id="city"
-            {...register("userAddress.city")}
-          />
-          {errors.userAddress?.city?.message && (
-            <span>{errors.userAddress?.city?.message}</span>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="state">Estado*</label>
-          <input
-            type="text"
-            name="state"
-            id="state"
-            {...register("userAddress.state")}
-          />
-          {errors.userAddress?.state?.message && (
-            <span>{errors.userAddress?.state?.message}</span>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="complement">Complemento</label>
-          <input
-            type="text"
-            name="complement"
-            id="complement"
-            {...register("userAddress.complement")}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="number">Número*</label>
-          <input
-            type="text"
-            name="number"
-            id="number"
-            {...register("userAddress.number")}
-          />
-          {errors.userAddress?.number?.message && (
-            <span>{errors.userAddress?.number?.message}</span>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="neighborhood">Bairro*</label>
-          <input
-            type="text"
-            name="neighborhood"
-            id="neighborhood"
-            {...register("userAddress.neighborhood")}
-          />
-          {errors.userAddress?.neighborhood?.message && (
-            <span>{errors.userAddress?.neighborhood?.message}</span>
-          )}
-        </div>
-
-        <button>Enviar</button>
-        <button type="reset" onClick={() => reset()}>
-          Resetar
-        </button>
-        <Link to={"/"}>Login</Link>
-        {registrationResult && <p>{registrationResult}</p>}
-      </S.Form>
-    </>
+    <Container>
+      <S.Wrapper>
+        <h2>Cadastrar</h2>
+        <S.Form onSubmit={handleSubmit(handleRegisterUser)}>
+          <InputWrapper>
+            <label htmlFor="fullName">Nome completo*</label>
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              {...register("fullName")}
+            />
+            {errors.fullName && <p>{errors.fullName.message}</p>}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="email">E-mail*</label>
+            <input type="text" name="email" id="email" {...register("email")} />
+            {errors.email && <p>{errors.email.message}</p>}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="photoUrl">URL foto de perfil</label>
+            <input
+              type="text"
+              name="photoUrl"
+              id="photoUrl"
+              {...register("photoUrl")}
+            />
+            {errors.photoUrl && <p>{errors.photoUrl.message}</p>}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="phone">Telefone*</label>
+            <input
+              type="phone"
+              name="phone"
+              id="phone"
+              {...register("phone")}
+            />
+            {errors.phone && <p>{errors.phone.message}</p>}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="password">Senha*</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              {...register("password")}
+            />
+            {errors.password && <p>{errors.password.message}</p>}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="passwordConfirmation">Confirmação de senha*</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              {...register("confirmPassword")}
+            />
+            {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="zipCode">CEP*</label>
+            <input
+              type="text"
+              name="zipCode"
+              id="zipCode"
+              {...register("userAddress.zipCode", {
+                onBlur: (e) => {
+                  const zipCode = e.target.value;
+                  const isValidZip = validatorRegex.zipCode.test(zipCode);
+                  if (isValidZip) {
+                    const numbersOnlyZip = zipCode.replace(/\D/g, "");
+                    fetch(`https://viacep.com.br/ws/${numbersOnlyZip}/json/`)
+                      .then((response) => response.json())
+                      .then(({ bairro, localidade, logradouro, uf }) => {
+                        setValue("userAddress.neighborhood", bairro);
+                        setValue("userAddress.city", localidade);
+                        setValue("userAddress.street", logradouro);
+                        setValue("userAddress.state", uf);
+                      });
+                  }
+                },
+              })}
+            />
+            {errors.userAddress?.zipCode?.message && (
+              <p>{errors.userAddress?.zipCode?.message}</p>
+            )}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="street">Logradouro/Endereço*</label>
+            <input
+              type="text"
+              name="street"
+              id="street"
+              {...register("userAddress.street")}
+            />
+            {errors.userAddress?.street?.message && (
+              <p>{errors.userAddress?.street.message}</p>
+            )}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="city">Cidade*</label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              {...register("userAddress.city")}
+            />
+            {errors.userAddress?.city?.message && (
+              <p>{errors.userAddress?.city?.message}</p>
+            )}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="state">Estado*</label>
+            <input
+              type="text"
+              name="state"
+              id="state"
+              {...register("userAddress.state")}
+            />
+            {errors.userAddress?.state?.message && (
+              <p>{errors.userAddress?.state?.message}</p>
+            )}
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="complement">Complemento</label>
+            <input
+              type="text"
+              name="complement"
+              id="complement"
+              {...register("userAddress.complement")}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="number">Número*</label>
+            <input
+              type="text"
+              name="number"
+              id="number"
+              {...register("userAddress.number")}
+            />
+            {errors.userAddress?.number?.message && (
+              <p>{errors.userAddress?.number?.message}</p>
+            )}
+          </InputWrapper>
+          <S.CustomInputWrapper>
+            <label htmlFor="neighborhood">Bairro*</label>
+            <input
+              type="text"
+              name="neighborhood"
+              id="neighborhood"
+              {...register("userAddress.neighborhood")}
+            />
+            {errors.userAddress?.neighborhood?.message && (
+              <p>{errors.userAddress?.neighborhood?.message}</p>
+            )}
+          </S.CustomInputWrapper>
+          {registrationResult && <p>{registrationResult}</p>}
+          <S.SendWrapper>
+            <Button type="submit">Enviar</Button>
+            <S.CustomLink to={"/"}>Login</S.CustomLink>
+          </S.SendWrapper>
+        </S.Form>
+      </S.Wrapper>
+    </Container>
   );
 };
--- src/pages/Registration/Registration.styles.js
+++ src/pages/Registration/Registration.styles.js
@@ -1,3 +1,61 @@
-import styled from "styled-components";
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { InputWrapper } from "@components";
 
-export const Form = styled.form``;
+export const SendWrapper = styled.div`
+  margin-top: 16px;
+  gap: 16px;
+  display: grid;
+  grid-template-columns: minmax(min-content, 200px);
+  place-self: center;
+  justify-content: center;
+  width: 100%;
+  grid-column: 1/-1;
+`;
+
+export const CustomLink = styled(Link)`
+  place-self: center;
+  ${({ theme }) => css`
+    color: ${theme.colors.main.main11};
+    width: fit-content;
+    text-decoration: underline;
+
+    &:hover {
+      color: ${theme.colors.main.main12};
+    }
+  `}
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 100px 0;
+  padding: 40px;
+  gap: 16px;
+
+  ${({ theme }) => css`
+    background-color: ${theme.colors.gray.gray01};
+    border-radius: ${theme.borderRadius};
+  `}
+
+  @media (max-width: 516px) {
+    padding: 20px 8px;
+  }
+`;
+
+export const Form = styled.form`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  row-gap: 28px;
+  column-gap: 16px;
+
+  @media (max-width: 516px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const CustomInputWrapper = styled(InputWrapper)`
+  grid-column: 1/-1;
+  grid-template-columns: minmax(200px, 100%);
+`;
