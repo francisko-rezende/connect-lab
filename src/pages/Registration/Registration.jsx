3a370f9d08be65e22001c48244cf7f343efc975c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-04 13:49:58 -0300

    refactor: start using useRegistrationFormHook in Registration

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,56 +1,16 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
 import * as S from "./Registration.styles.js";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { axiosInstance } from "@lib/axios";
-import { formSchema } from "@lib/yup";
 import { Button, Container, TextField } from "@components";
-import toast, { Toaster } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useRegistrationForm } from "@hooks";
 
 export const Registration = () => {
   const {
-    register,
+    errors,
+    handleRegisterUser,
     handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      address: {
-        state: "RJ",
-        city: "Rio de Janeiro",
-        neighborhood: "Vale dos Puxadores de Ferro",
-        number: 17,
-        street: "Rua Felipe Franco",
-        zipCode: "28495-000",
-        complement: "",
-      },
-      phone: "(35) 9 9828-0000",
-      photoUrl: "https://duck.com",
-      fullName: "Léo Stronda",
-      confirmPassword: "VemMonstro",
-      password: "VemMonstro",
-      email: "leo@stronda.com",
-    },
-    resolver: yupResolver(formSchema),
-  });
-
-  const navigate = useNavigate();
-  const [registrationResult, setRegistrationResult] = useState("");
-
-  const handleRegisterUser = async (data) => {
-    try {
-      await axiosInstance.post("/auth/register", data);
-      toast.success("Cadastro realizado com sucesso!");
-      setTimeout(() => navigate("/login"), 3 * 1000);
-    } catch (error) {
-      if (error.response.status === 409) {
-        setRegistrationResult("Usuário já registrado.");
-        return;
-      }
-
-      setRegistrationResult("Houve um erro, tente novamente mais tarde");
-    }
-  };
+    register,
+    registrationResult,
+  } = useRegistrationForm();
 
   return (
     <Container>
