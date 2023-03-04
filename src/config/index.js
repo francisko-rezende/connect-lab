d8fafc1f4251fb696eac438aa7854075105dcd03
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-04 13:52:13 -0300

    refactor: move viacep base url to .env

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 // todo create env var for weather base api?
 
 export const API_URL = import.meta.env.VITE_API_BASE_URL;
+export const VIACEP_URL = import.meta.env.VITE_VIACEP_URL;
 export const OPENWEATHER_KEY = import.meta.env.VITE_OPENWEATHER_KEY;
